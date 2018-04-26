82582f4d0b0ac52b7159dd8cccda49d34a70c84a
Michal 'vorner' Vaner <vorner@vorner.cz>
2018-04-26 18:42:09 +0200

    Fix armadillo
    To use floats, not doubles.

--- 3rd-party-lib/armadillo.cpp
+++ 3rd-party-lib/armadillo.cpp
@@ -4,16 +4,16 @@
 
 int main() {
 
-	for (size_t i = 10; i < 14; i ++) {
+	for (size_t i = 10; i < 15; i ++) {
 		std::cout << "Generating matrices" << std::endl;
 		size_t size = 1 << i;
-		arma::mat a = arma::randu<arma::mat>(size, size);
-		arma::mat b = arma::randu<arma::mat>(size, size);
+		arma::fmat a = arma::randu<arma::fmat>(size, size);
+		arma::fmat b = arma::randu<arma::fmat>(size, size);
 
 		std::cout << "Starting armadillo" << std::endl;
 		std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
 
-		arma::mat c = a * b;
+		arma::fmat c = a * b;
 
 		size_t millis = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start).count();
 		size_t secs = millis / 1000;
